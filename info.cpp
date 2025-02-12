3fc3ac3284ba97ed55138bda0439657930ddbace
Erys Kakdam <123325727+mahdiyarshem@users.noreply.github.com>
2025-02-12 17:17:44 +0330

    Add files via upload

---
+++ info.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+class User {
+public:
+    std::string username;
+    std::string password;
+    std::string skin;
+    int playtime; // زمان بازی به ساعت
+
+    User(std::string uname, std::string pwd, std::string skn, int pt)
+        : username(uname), password(pwd), skin(skn), playtime(pt)) {}
+};
